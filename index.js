d24b6fc1a2e51f9e5ffc5c1182d12712edbdb935
michaelwachira707@gmail.com <michaelwachira707@gmail.com>
2025-03-12 18:16:38 +0300

    lab done

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    function namedFunction() {
+      return "This is a named function.";
+    }
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      return "This is an anonymous function.";
+    };
+  }
+  
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction,
+  };
